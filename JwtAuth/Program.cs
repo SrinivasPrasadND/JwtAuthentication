aa89e746000ae5e8acb45c2a57fadbe0ac9b70e8
SrinivasPrasad <srinivasprasad.ndreddy@gmail.com>
2022-07-14 07:47:55 +0530

    Sawgger config added

--- JwtAuth/Program.cs
+++ JwtAuth/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +22,43 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder
+    .Services.AddSwaggerGen(swagger =>
+    {   
+        //This is to generate the Default UI of Swagger Documentation  
+        swagger.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Version = "v1",
+            Title = "JWT Token Authentication API",
+            Description = ".NET6 API"
+        });
+        // To Enable authorization using Swagger (JWT)  
+        swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+        {
+            Name = "Authorization",
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+        });
+        swagger.AddSecurityRequirement(
+            new OpenApiSecurityRequirement
+            {
+                {   new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] {} 
+                }
+            });
+    });
+
 
 //var key = Guid.NewGuid().ToString();
 builder.Services.AddAuthentication(x =>
